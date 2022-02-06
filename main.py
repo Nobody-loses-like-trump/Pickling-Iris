58a6451aff38e98ac0c2f06c1967498118e999d9
Nobody-loses-like-trump <87992980+Nobody-loses-like-trump@users.noreply.github.com>
2022-02-06 11:37:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 r = requests.get("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
 data = r.text
 
-list_of_lists = [[y for y in x.split(",")] for x in data.split("\n")]
+list_of_lists = [[y for y in x.split(",")] for x in data.splitlines() if x != ""]
 
 with open("Iris.pkl", "wb") as f:
     pickle.dump(list_of_lists, f)
